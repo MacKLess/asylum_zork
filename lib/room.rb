1efc79c6c457f148f4c408d7364c134976e9eaf4
Kelsey Langlois and LDMacKrell <ldmackrell@>
2017-10-02 11:46:24 -0700

    Add find_by_coordinates method to room class.

--- lib/room.rb
+++ lib/room.rb
@@ -3,6 +3,10 @@
 class Room < ActiveRecord::Base
 
   def move(direction)
+    direction.downcase!
+    if (direction == "north") & north_exit
+      # return room with coordinates one north
+    end
   end
 
   def look
@@ -16,5 +20,14 @@ class Room < ActiveRecord::Base
   end
 
   def read(note)
+  end
+
+  def self.find_by_coordinates(x, y)
+    results = Room.where("x_coordinate = ? AND y_coordinate = ? AND active = ?", x, y, true)
+    if results.length > 0
+      return results.first
+    else
+      return nil
+    end
   end
 end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe('Room') do
-  let(:room) { Room.create({
+  let(:room) { Room.new({
     name: 'Start',
     description: 'The First Room.',
     x_coordinate: 1,
@@ -16,9 +16,23 @@ describe('Room') do
     south_exit: true,
     west_exit: true,
   }) }
+
   describe '#look' do
     it "returns the room's description" do
+      room.save
       expect(room.look).to eq('The First Room.')
+    end
+  end
+
+  describe '.find_by_coordinates' do
+    it "returns the active room with the coordinates passed in" do
+      room.save
+      expect(Room.find_by_coordinates(1, 1)).to eq(room)
+    end
+
+    it "returns nil if no active room with the coordinates passed in" do
+      room.save
+      expect(Room.find_by_coordinates(0, 0)).to eq(nil)
     end
   end
 end
